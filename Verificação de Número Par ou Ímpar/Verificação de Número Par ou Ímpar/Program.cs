4b707730aa10e36403e96382f5f1b481c6d01b9a
Pedro Henrique Gaspar <mortadela22112005@gmail.com>
2023-08-15 09:42:38 -0300

    par ou impar

---
+++ Verificação de Número Par ou Ímpar/Verificação de Número Par ou Ímpar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verificação_de_Número_Par_ou_Ímpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float  num ;
+
+            Console.WriteLine("Selecione o numero que deseja para a verificação: ");
+
+            num = float.Parse(Console.ReadLine());
+
+            
+          
+
+            if ( num % 2 == 0)
+            {
+
+                Console.WriteLine("ele é par:");
+
+            }else 
+            {
+                Console.WriteLine("Ele é impar:");
+            
+            }
+
+            Console.ReadKey();  
+
+        }
+    }
+}
